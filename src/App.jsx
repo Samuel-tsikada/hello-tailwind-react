869c8a2c1b6222613bb67436b1f777ebf1dc305d
Samuel-tsikada <samuel@it-savanna.com>
2025-11-25 11:50:49 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pink-500">
+      <h1 className="text-6xl text-white font-bold">Tailwind Works!</h1>
+    </div>
+  )
+}
+
+export default App;
